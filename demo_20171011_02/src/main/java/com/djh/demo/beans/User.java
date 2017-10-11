9312d760237aa467e82f723bf78db1533bed760c
duanjunhua <1585115894@qq.com>
2017-10-11 10:38:20 +0800

    Springboot implement Mybatis Add,Delete,Update,Query and use Mockito for unit test.

---
+++ demo_20171011_02/src/main/java/com/djh/demo/beans/User.java
@@ -0,0 +1,50 @@
+package com.djh.demo.beans;
+
+public class User {
+
+	private int id;
+	private String userName;
+	private String password;
+	private String email;
+	private String description;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
