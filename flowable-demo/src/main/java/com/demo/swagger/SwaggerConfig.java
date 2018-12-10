a3d77a4c91621f6d86053890647ef4bbed335ca1
Administrator <Administrator@PC-201707041527>
2018-12-10 15:37:43 +0800

    SpringBoot整合Flowable实现工作流

---
+++ flowable-demo/src/main/java/com/demo/swagger/SwaggerConfig.java
@@ -0,0 +1,50 @@
+package com.demo.swagger;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Component
+@Configuration
+public class SwaggerConfig {
+
+	@Value("${spring.cloud.config.profile}")
+	private String env;
+	
+	public static final String[] TEST_ENV = {"DEV", "SIT", "UAT"};
+	
+	@Bean
+	public Docket swagger() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.enable(nonePrdEnv())
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.demo.controller"))
+				.paths(PathSelectors.any())				
+				.build();
+	}
+	
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Test Swagger API")
+				.contact(new Contact("Michael J H Duan", "http://www.baidu.com", "michael@test.com"))
+				.description("Test Demo")
+				.version("v0.1")
+				.termsOfServiceUrl("http://www,baidu.com")
+				.build();
+	}
+	
+	private Boolean nonePrdEnv() {
+		return ArrayUtils.contains(SwaggerConfig.TEST_ENV, env.toUpperCase()) ;
+	}
+}
